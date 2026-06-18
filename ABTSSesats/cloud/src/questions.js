0eb788b2917dc7d00c857ecd7de395ce14b0abb2
Edward Bender <ebender001@gmail.com>
2026-06-18 10:53:14 -0500

    questions are now always returned with their options (distractors + correct answer)

--- ABTSSesats/cloud/src/questions.js
+++ ABTSSesats/cloud/src/questions.js
@@ -1,4 +1,5 @@
 const Question = Parse.Object.extend("Question");
+const QuestionOption = Parse.Object.extend("QuestionOption");
 const ParseUser = Parse.User;
 
 function requireString(value, fieldName) {
@@ -45,6 +46,18 @@ function normalizeOptionalDate(value, fieldName) {
   }
 
   return date;
+}
+
+function normalizeOptionalNumber(value, fieldName) {
+  if (value === undefined || value === null) {
+    return undefined;
+  }
+
+  if (typeof value !== "number" || Number.isNaN(value)) {
+    throw new Parse.Error(Parse.Error.VALIDATION_ERROR, `${fieldName} must be a number.`);
+  }
+
+  return value;
 }
 
 function setUserPointer(question, fieldName, objectId) {
@@ -149,12 +162,118 @@ function serializeQuestion(question) {
   };
 }
 
+async function fetchQuestionOptionsByQuestionIds(questionIds) {
+  if (!questionIds.length) {
+    return new Map();
+  }
+
+  const questionPointers = questionIds.map((questionId) => {
+    const question = new Question();
+    question.id = questionId;
+    return question;
+  });
+
+  const query = new Parse.Query(QuestionOption);
+  query.containedIn("question", questionPointers);
+  query.ascending("sortOrder");
+  query.ascending("label");
+  query.limit(1000);
+
+  const options = await query.find({ useMasterKey: true });
+  const optionsByQuestionId = new Map();
+
+  options.forEach((option) => {
+    const questionPointer = option.get("question");
+    const questionId = questionPointer ? questionPointer.id : "";
+
+    if (!optionsByQuestionId.has(questionId)) {
+      optionsByQuestionId.set(questionId, []);
+    }
+
+    optionsByQuestionId.get(questionId).push(serializeQuestionOption(option));
+  });
+
+  return optionsByQuestionId;
+}
+
+async function serializeQuestionWithOptions(question) {
+  const serializedQuestion = serializeQuestion(question);
+  const optionsByQuestionId = await fetchQuestionOptionsByQuestionIds([question.id]);
+
+  return {
+    ...serializedQuestion,
+    questionOptions: optionsByQuestionId.get(question.id) || [],
+  };
+}
+
+async function serializeQuestionsWithOptions(questions) {
+  const serializedQuestions = questions.map(serializeQuestion);
+  const optionsByQuestionId = await fetchQuestionOptionsByQuestionIds(
+    questions.map((question) => question.id)
+  );
+
+  return serializedQuestions.map((question) => ({
+    ...question,
+    questionOptions: optionsByQuestionId.get(question.objectId) || [],
+  }));
+}
+
+function setQuestionPointer(option, questionObjectId) {
+  if (questionObjectId === null || questionObjectId === "") {
+    option.unset("question");
+    return;
+  }
+
+  const question = new Question();
+  question.id = requireString(questionObjectId, "questionObjectId");
+  option.set("question", question);
+}
+
+function applyQuestionOptionFields(
+  option,
+  params,
+  { requireQuestion = false, requireLabel = false, requireText = false } = {}
+) {
+  if (requireQuestion || Object.prototype.hasOwnProperty.call(params, "questionObjectId")) {
+    setQuestionPointer(option, params.questionObjectId);
+  }
+
+  if (requireLabel || Object.prototype.hasOwnProperty.call(params, "label")) {
+    option.set("label", requireString(params.label, "label"));
+  }
+
+  if (requireText || Object.prototype.hasOwnProperty.call(params, "text")) {
+    option.set("text", requireString(params.text, "text"));
+  }
+
+  if (Object.prototype.hasOwnProperty.call(params, "isCorrect")) {
+    option.set("isCorrect", normalizeOptionalBoolean(params.isCorrect, "isCorrect"));
+  }
+
+  if (Object.prototype.hasOwnProperty.call(params, "sortOrder")) {
+    option.set("sortOrder", normalizeOptionalNumber(params.sortOrder, "sortOrder"));
+  }
+}
+
+function serializeQuestionOption(option) {
+  return {
+    objectId: option.id,
+    questionObjectId: serializePointer(option.get("question")),
+    label: option.get("label") || "",
+    text: option.get("text") || "",
+    isCorrect: option.get("isCorrect"),
+    sortOrder: option.get("sortOrder"),
+    createdAt: serializeDate(option.createdAt),
+    updatedAt: serializeDate(option.updatedAt),
+  };
+}
+
 Parse.Cloud.define("addQuestion", async (request) => {
   const question = new Question();
   applyQuestionFields(question, request.params || {}, { requireStem: true });
 
   const savedQuestion = await question.save(null, { useMasterKey: true });
-  return serializeQuestion(savedQuestion);
+  return serializeQuestionWithOptions(savedQuestion);
 });
 
 Parse.Cloud.define("getQuestion", async (request) => {
@@ -162,7 +281,7 @@ Parse.Cloud.define("getQuestion", async (request) => {
   const query = new Parse.Query(Question);
   const question = await query.get(objectId, { useMasterKey: true });
 
-  return serializeQuestion(question);
+  return serializeQuestionWithOptions(question);
 });
 
 Parse.Cloud.define("editQuestion", async (request) => {
@@ -173,7 +292,7 @@ Parse.Cloud.define("editQuestion", async (request) => {
   applyQuestionFields(question, request.params || {});
 
   const savedQuestion = await question.save(null, { useMasterKey: true });
-  return serializeQuestion(savedQuestion);
+  return serializeQuestionWithOptions(savedQuestion);
 });
 
 Parse.Cloud.define("deleteQuestion", async (request) => {
@@ -196,5 +315,68 @@ Parse.Cloud.define("listQuestions", async () => {
   query.limit(1000);
 
   const questions = await query.find({ useMasterKey: true });
-  return questions.map(serializeQuestion);
+  return serializeQuestionsWithOptions(questions);
+});
+
+Parse.Cloud.define("addQuestionOption", async (request) => {
+  const option = new QuestionOption();
+  applyQuestionOptionFields(option, request.params || {}, {
+    requireQuestion: true,
+    requireLabel: true,
+    requireText: true,
+  });
+
+  const savedOption = await option.save(null, { useMasterKey: true });
+  return serializeQuestionOption(savedOption);
+});
+
+Parse.Cloud.define("getQuestionOption", async (request) => {
+  const objectId = requireString(request.params.objectId, "objectId");
+  const query = new Parse.Query(QuestionOption);
+  const option = await query.get(objectId, { useMasterKey: true });
+
+  return serializeQuestionOption(option);
+});
+
+Parse.Cloud.define("editQuestionOption", async (request) => {
+  const objectId = requireString(request.params.objectId, "objectId");
+  const query = new Parse.Query(QuestionOption);
+  const option = await query.get(objectId, { useMasterKey: true });
+
+  applyQuestionOptionFields(option, request.params || {});
+
+  const savedOption = await option.save(null, { useMasterKey: true });
+  return serializeQuestionOption(savedOption);
+});
+
+Parse.Cloud.define("deleteQuestionOption", async (request) => {
+  const objectId = requireString(request.params.objectId, "objectId");
+  const query = new Parse.Query(QuestionOption);
+  const option = await query.get(objectId, { useMasterKey: true });
+
+  await option.destroy({ useMasterKey: true });
+
+  return {
+    objectId,
+    deleted: true,
+  };
+});
+
+Parse.Cloud.define("listQuestionOptions", async (request) => {
+  const params = request.params || {};
+  const query = new Parse.Query(QuestionOption);
+
+  if (Object.prototype.hasOwnProperty.call(params, "questionObjectId")) {
+    const questionObjectId = requireString(params.questionObjectId, "questionObjectId");
+    const question = new Question();
+    question.id = questionObjectId;
+    query.equalTo("question", question);
+  }
+
+  query.ascending("sortOrder");
+  query.ascending("label");
+  query.limit(1000);
+
+  const options = await query.find({ useMasterKey: true });
+  return options.map(serializeQuestionOption);
 });
