2a5f743d958b666f7477b4fe423564e40bcde833
Edward Bender <ebender001@gmail.com>
2026-06-18 10:17:21 -0500

    set up for local development and b4a deploy

---
+++ ABTSSesats/cloud/main.js
@@ -0,0 +1,8 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", (request) => {
+	//thus is a test
+	
+	return "Hello world!";
+});
